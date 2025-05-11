2fc447e3fddc0483912a5e669fb6b4e9f1cc0569
craig white <craig.t.w@hotmail.co.uk>
2025-05-11 20:46:03 +0100

    added script to move red block

---
+++ Assets/MoveRedBlock2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MoveRedBlock2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //move red block 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
